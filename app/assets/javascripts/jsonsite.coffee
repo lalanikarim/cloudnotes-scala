0b4fd7c2ca5e713895067d1e5c2523c15baf9112
Karim Lalani <karim@karim-lalani.com>
2014-03-29 21:15:25 -0500

    Converted more features to AngularJS Added $interval support to refresh notes every 5 secs Created angular directive to handle note - draggable, resizable, etc

--- app/assets/javascripts/jsonsite.coffee
+++ app/assets/javascripts/jsonsite.coffee
@@ -1,4 +1,44 @@
 notesApp = angular.module "notesApp",[]
+notesApp.directive 'note', ($http,$interval) ->
+	restrict: 'E',templateUrl: '/assets/angular/views/note.html', link : (scope, element, attrs) ->
+		
+		element.draggable({
+			start: () ->
+				scope.stopTimer()
+			stop: () ->
+				$http({method: 'POST', url: '/note/saveposition', data:{id: element.attr('id'),top: element.position().top, left: element.position().left}}).success (data, status, headers, config) ->
+					scope.note = data
+					scope.startTimer()
+		})
+		element.resizable({
+			stop: () ->
+				$http({method: 'POST', url: '/note/savedimension', data:{id: element.attr('id'),height: element.height(), width: element.width()}}).success (data, status, headers, config) ->
+					scope.note = data
+					scope.startTimer()
+		})
+		element.find('.delete-note').first().bind 'click', () ->
+			$http({method: 'DELETE', url: '/note/' + scope.note.Id}).success () ->
+				scope.stopTimer()
+				element.fadeOut 500
+				scope.$destroy()
+			
+		
+		scope.startTimer = () ->
+			scope.stop = $interval () ->
+				$http({method: 'GET', url: '/note/' + scope.note.Id}).success (data, status, headers, config) ->
+						scope.note = data
+						scope.startTimer()
+						true
+			, 5000, 1
+			true
+		scope.stopTimer = () ->
+			$interval.cancel scope.stop
+		scope.startTimer()
+		
+		scope.$on '$destroy', () ->
+			scope.stopTimer()
+		true
+		
 notesApp.controller "NotesCtrl", ($scope,$http) ->
 	$http.get('/note/json').success (data) ->
 		$scope.notes = data;
@@ -7,25 +47,7 @@ notesApp.controller "NotesCtrl", ($scope,$http) ->
 			return note.height - 35
 		else
 			return 35;
-$('.note').resizable(
-#	{
-#    stop: () ->
-#      $("#notedimensionid").val $(this).attr "id"
-#      $("#notedimensionheight").val $(this).height()
-#      $("#notedimensionwidth").val $(this).width()
-#      $("#notedimension").submit()
-#	}
-)
-$('.note').draggable(
-#	{
-#    cancel: ".editable, .note-body",
-#    stop: () ->
-#      $("#notepositionid").val $(this).attr "id"
-#      $("#notepositiontop").val $(this).position().top
-#      $("#notepositionleft").val $(this).position().left
-#      $("#noteposition").submit()
-#	}
-)
+
 $('textarea.editable').each () ->
 	resizetextarea this
    
--- app/controllers/NoteController.java
+++ app/controllers/NoteController.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import org.codehaus.jackson.JsonNode;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import models.Note;
 import models.NoteItem;
 import net.vz.mongodb.jackson.DBUpdate;
@@ -52,6 +54,7 @@ public class NoteController extends Controller {
 		return redirect("/");
 	}
 	
+	@BodyParser.Of(BodyParser.Json.class)
 	public static Result saveposition(){
 		Map<String,String> data = dynamicForm.bindFromRequest().data();
 		String id = data.get("id");
@@ -59,9 +62,18 @@ public class NoteController extends Controller {
 		String top = data.get("top");
 		
 		notes.updateById(id, DBUpdate.set("left", Math.floor(Double.parseDouble(left))).set("top", Math.floor( Double.parseDouble(top))));
-		return redirect("/");
+		
+		Note note = notes.findOneById(id);
+		com.fasterxml.jackson.databind.JsonNode result = Json.toJson(note);
+		//result.put("status", "success");
+		//if(isXmlHttp()){
+			return ok(result);
+		//} else {
+		//	return redirect("/");
+		//}
 	}
 	
+	@BodyParser.Of(BodyParser.Json.class)
 	public static Result savedimension(){
 		Map<String,String> data = dynamicForm.bindFromRequest().data();
 		String id = data.get("id");
@@ -69,7 +81,27 @@ public class NoteController extends Controller {
 		String width = data.get("width");
 		
 		notes.updateById(id, DBUpdate.set("height", Math.floor(Double.parseDouble(height))).set("width", Math.floor( Double.parseDouble(width))));
-		return redirect("/");
+		Note note = notes.findOneById(id);
+		
+		com.fasterxml.jackson.databind.JsonNode result = Json.toJson(note);
+		//if(isXmlHttp()){
+			return ok(result);
+		//} else {
+		//	return redirect("/");
+		//}
+	}
+	
+	@BodyParser.Of(BodyParser.Json.class)
+	public static Result getNote(String noteid){
+		
+		Note note = notes.findOneById(noteid);
+		
+		com.fasterxml.jackson.databind.JsonNode result = Json.toJson(note);
+		//if(isXmlHttp()){
+			return ok(result);
+		//} else {
+		//	return redirect("/");
+		//}
 	}
 
 	public static Result saveitem(String noteid){
@@ -119,5 +151,10 @@ public class NoteController extends Controller {
     	com.fasterxml.jackson.databind.JsonNode result = Json.toJson(coll.find().toArray());
     	
     	return ok(result);
+    }
+    
+    private static boolean isXmlHttp(){
+    	return request().getHeader("X-Requested-With") != null
+            	&& request().getHeader("X-Requested-With").equals("XMLHttpRequest");
     }
 }
